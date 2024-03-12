9169cc02003e86c4c3f0649b8cab1bdc7cfd4654
tharindu-kaiju <tharindu@kaiju3d.com>
2024-03-12 15:31:08 +0530

    add prod key

--- ZerpmonAnimations/generateSpritesheet.js
+++ ZerpmonAnimations/generateSpritesheet.js
@@ -1,42 +1,42 @@
 let texturePacker = require("free-tex-packer-core");
-const fs = require('fs');
+const fs = require("fs");
 
 const args = process.argv.slice(2);
 
 if (args.length < 1) {
-    console.error('Usage: node generateSpritesheet.js <textureName>');
-    process.exit(1);
+  console.error("Usage: node generateSpritesheet.js <textureName>");
+  process.exit(1);
 }
 
 const textureName = args[0];
-const pngSequencePath = `pngSequences/${textureName}`
+const pngSequencePath = `pngSequences/${textureName}`;
 const spritesheetPath = `Spritesheets/${textureName}`;
 
 let options = {
-    // textureName: "equipAppear",
-    textureName: textureName,
-    textureFormat: "png",   //defualt
-    exporter: "Phaser3",
-    removeFileExtension: false, //default
-    prependFolderName: false,   //defualt
-    base64Export: false,    //defualt
-    tinify: true,  //defualt
-    tinifyKey: "mZmdM3QSN180BlBXDBdkh5jK5Z1LsQ7J",  //defualt
-    scale: 1,   //defualt
-    // filter: none //default
-    width: 4000,
-    height: 4000,
-    fixedSize: false,   //default
-    powerOfTwo: false,  //default
-    padding: 0,     //default
-    extrude: 0,     //default
-    allowRotation: false,   //default
-    allowTrim: true,    //default
-    //trimMode: trim,   //default
-    alphaThreshold: 16,
-    detectIdentical: true,  //default
-    packer: "MaxRectsBin",
-    packerMethod: "BestLongSideFit",
+  // textureName: "equipAppear",
+  textureName: textureName,
+  textureFormat: "png", //defualt
+  exporter: "Phaser3",
+  removeFileExtension: false, //default
+  prependFolderName: false, //defualt
+  base64Export: false, //defualt
+  tinify: true, //defualt
+  tinifyKey: "HgMHbnBKj5x2Fq7GH2TPcKJSRDwbMdy9", //defualt
+  scale: 1, //defualt
+  // filter: none //default
+  width: 4000,
+  height: 4000,
+  fixedSize: false, //default
+  powerOfTwo: false, //default
+  padding: 0, //default
+  extrude: 0, //default
+  allowRotation: false, //default
+  allowTrim: true, //default
+  //trimMode: trim,   //default
+  alphaThreshold: 16,
+  detectIdentical: true, //default
+  packer: "MaxRectsBin",
+  packerMethod: "BestLongSideFit",
 };
 
 let images = [];
@@ -44,35 +44,38 @@ let images = [];
 const files = fs.readdirSync(pngSequencePath);
 
 for (const file of files) {
-    const filePath = `${pngSequencePath}/${file}`;
+  const filePath = `${pngSequencePath}/${file}`;
 
-    // Check if the item is a file (not a directory)
-    const isFile = fs.statSync(filePath).isFile();
+  // Check if the item is a file (not a directory)
+  const isFile = fs.statSync(filePath).isFile();
 
-    if (isFile) {
-        images.push({ path: filePath, contents: fs.readFileSync(filePath) });
-    }
+  if (isFile) {
+    images.push({ path: filePath, contents: fs.readFileSync(filePath) });
+  }
 }
 
 if (!fs.existsSync(spritesheetPath)) {
-    fs.mkdirSync(spritesheetPath, { recursive: true });
+  fs.mkdirSync(spritesheetPath, { recursive: true });
 }
 
 texturePacker(images, options, (files, error) => {
-    if (error) {
-        console.error('Packaging failed', error);
-    } else {  
-        for (let item of files) {
-            // Writing PNG file
-            if (item.name.endsWith('.png')) {
-                fs.writeFileSync(`${spritesheetPath}/${item.name}`, item.buffer);
-                console.log(`${item.name} written successfully.`);
-            }
-            // Writing JSON file
-            else if (item.name.endsWith('.json')) {
-                fs.writeFileSync(`${spritesheetPath}/${item.name}`, JSON.stringify(JSON.parse(item.buffer.toString()), null, 2));
-                console.log(`${item.name} written successfully.`);
-            }
-        }
+  if (error) {
+    console.error("Packaging failed", error);
+  } else {
+    for (let item of files) {
+      // Writing PNG file
+      if (item.name.endsWith(".png")) {
+        fs.writeFileSync(`${spritesheetPath}/${item.name}`, item.buffer);
+        console.log(`${item.name} written successfully.`);
+      }
+      // Writing JSON file
+      else if (item.name.endsWith(".json")) {
+        fs.writeFileSync(
+          `${spritesheetPath}/${item.name}`,
+          JSON.stringify(JSON.parse(item.buffer.toString()), null, 2)
+        );
+        console.log(`${item.name} written successfully.`);
+      }
     }
+  }
 });
