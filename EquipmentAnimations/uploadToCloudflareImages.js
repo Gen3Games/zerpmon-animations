ab2cad0b3b774cb762064dd6c03940f7a80bbfb7
JadenLR <jadenjanz@liveroom.dev>
2025-10-02 18:02:20 +0530

    change env keys

--- EquipmentAnimations/uploadToCloudflareImages.js
+++ EquipmentAnimations/uploadToCloudflareImages.js
@@ -19,13 +19,12 @@ async function uploadToCloudFlareImages() {
         formData.append("file", fileContent, { filename: file });
         formData.append("id", file.split(".")[0]);
 
-        let url =
-          "https://api.cloudflare.com/client/v4/accounts/f2183aa9a7cd9634f52e696950d49f06/images/v1";
+        let url = `https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/images/v1`;
 
         let options = {
           method: "POST",
           headers: {
-            Authorization: process.env.CLOUDFLARE_IMAGES_KEY,
+            Authorization: `Bearer ${process.env.CLOUDFLARE_IMAGES_KEY}`,
           },
           body: formData,
         };
--- ZerpmonAnimations/uploadToCloudflareImages.js
+++ ZerpmonAnimations/uploadToCloudflareImages.js
@@ -25,12 +25,11 @@ async function uploadToCloudFlareImages(zerpmonNumber) {
         "id",
         `${zerpmonNumber}-${type}-${scale}-spritesheet.png`
       );
-      let url =
-        "https://api.cloudflare.com/client/v4/accounts/f2183aa9a7cd9634f52e696950d49f06/images/v1";
+      let url = `https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/images/v1`;
       let options = {
         method: "POST",
         headers: {
-          Authorization: process.env.CLOUDFLARE_IMAGES_KEY,
+          Authorization: `Bearer ${process.env.CLOUDFLARE_IMAGES_KEY}`,
         },
         body: formData,
       };
