c310ddaea74518703d86e6db73f4b1fc5da74c91
JadenLR <jadenjanz@liveroom.dev>
2025-10-02 18:05:50 +0530

    changes

--- EquipmentAnimations/uploadToCloudflare.js
+++ EquipmentAnimations/uploadToCloudflare.js
@@ -10,8 +10,7 @@ const fileContent = fs.readFileSync(
 formData.append("file", fileContent);
 formData.append("id", "1234567");
 
-let url =
-  "https://api.cloudflare.com/client/v4/accounts/f2183aa9a7cd9634f52e696950d49f06/images/v1";
+let url = `https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/images/v1`;
 
 let options = {
   method: "POST",
