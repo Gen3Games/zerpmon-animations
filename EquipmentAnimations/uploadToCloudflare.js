de88adf692e85dcc5899045e0f9e07ecbe2e14af
JadenLR <jadenjanz@liveroom.dev>
2025-10-02 17:46:38 +0530

    changes

--- EquipmentAnimations/uploadToCloudflare.js
+++ EquipmentAnimations/uploadToCloudflare.js
@@ -1,24 +1,27 @@
-const FormData = require('form-data');
-const fetch = require('node-fetch');
-const fs = require('fs');
+const FormData = require("form-data");
+const fetch = require("node-fetch");
+const fs = require("fs");
 
 const formData = new FormData();
-const fileContent = fs.readFileSync('./bug-equipment-spritesheet/bug-equipment-spritesheet/bug-equipment-spritesheet.png');
+const fileContent = fs.readFileSync(
+  "./bug-equipment-spritesheet/bug-equipment-spritesheet/bug-equipment-spritesheet.png"
+);
 
-formData.append('file', fileContent);
-formData.append('id', '1234567')
+formData.append("file", fileContent);
+formData.append("id", "1234567");
 
-let url = 'https://api.cloudflare.com/client/v4/accounts/f2183aa9a7cd9634f52e696950d49f06/images/v1';
+let url =
+  "https://api.cloudflare.com/client/v4/accounts/f2183aa9a7cd9634f52e696950d49f06/images/v1";
 
 let options = {
-  method: 'POST',
+  method: "POST",
   headers: {
-    Authorization: 'Bearer jDVp8XqiWkFijaCF55gEmqYGnjrtt1bXSmSFFzH6'
+    Authorization: process.env.CLOUDFLARE_IMAGES_KEY,
   },
-  body: formData
+  body: formData,
 };
 
 fetch(url, options)
-  .then(res => res.json())
-  .then(json => console.log(json))
-  .catch(err => console.error('error:' + err));
+  .then((res) => res.json())
+  .then((json) => console.log(json))
+  .catch((err) => console.error("error:" + err));
